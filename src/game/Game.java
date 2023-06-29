aaf769df80acf8b59f088c69bdbe92fa99a54f1a
grimli <73295986+grimlitv@users.noreply.github.com>
2023-06-28 23:01:07 -0400

    Fin
    Its dirty, but snake works.

--- src/game/Game.java
+++ src/game/Game.java
@@ -6,46 +6,58 @@ import util.Displayable;
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.util.LinkedList;
+import java.util.Random;
+import java.util.Vector;
 
 public class Game extends Displayable {
+    private int snakeSize = 20;
+    private int maxX = Options.SCREEN_WIDTH / snakeSize;
+    private int maxY = Options.SCREEN_HEIGHT / snakeSize;
     private Direction currentDirection = Direction.RIGHT;
     private Direction lastMove = Direction.RIGHT;
-    private int snakeSize = 40;
+
     private Color snakeColor = Globals.niceBlue;
     private Color foodColor = Color.YELLOW;
     private int snakeRow = Options.SCREEN_HEIGHT / snakeSize;
     private int snakeCol = Options.SCREEN_WIDTH / snakeSize;
 
-    private int grow = 20;
+    private boolean foodSpawned = false;
+    private VectorSnake foodLocation = null;
+
+    private int grow = 3;
 
     private int score = 0 - grow;
-    private LinkedList<SnakeSegment> snake = new LinkedList();
+    private LinkedList<VectorSnake> snake = new LinkedList();
 
     private double lastMovedDt = 0;
     // adjusting this sets the time between updates/moves for snake, in seconds?
-    private double howFast = .5;
+    private double howFast = .2;
+
+    private boolean goodGame = true;
+
+
 
     public Game() {
         spawnSnake();
-
-        // collision detection - with snake and border (wraparound setting?)
-        // food generation
-
-        // scoring? (hiscores/names)
         // speed button like shift? (settings?) (maybe adds to score?)
         // difficulty settings? (adjusts speed)
         // set snake size in settings (adjusts how big window is)
     }
 
     private void spawnSnake() {
-        SnakeSegment newSegment = new SnakeSegment(1, 1);
+        VectorSnake newSegment = new VectorSnake(1, 1);
         snake.addFirst(newSegment);
     }
 
     @Override
     public void update(double dt) {
         if (Globals.kl.isKeyReleased(KeyEvent.VK_ESCAPE)) {
-            Globals.ph.pauseScene(this);
+            if (!goodGame) {
+                System.out.println("YOU LOSE, score: " + score);
+                Globals.ph.switchScene(Scene.MAIN);
+            } else {
+                Globals.ph.pauseScene(this);
+            }
         } else if (Globals.kl.isKeyPressed(KeyEvent.VK_UP)) {
             if (currentDirection != Direction.DOWN) {
                 currentDirection = Direction.UP;
@@ -65,7 +77,7 @@ public class Game extends Displayable {
             }
         }
         lastMovedDt += dt;
-        if (lastMovedDt >= howFast) {
+        if (lastMovedDt >= howFast && goodGame) {
             lastMovedDt = 0;
             // set this as the new head
             snake.addFirst(getNewSnakeHead());
@@ -74,55 +86,77 @@ public class Game extends Displayable {
                 score++;
                 grow--;
             } else {
+                if (!foodSpawned) {
+                    makeFood();
+                }
                 snake.removeLast();
             }
         }
     }
 
-    private SnakeSegment getNewSnakeHead() {
+    private void makeFood() {
+        // generate a random x and a random y between 0 and maxX/maxY
+        while (!foodSpawned) {
+            Random rand = new Random();
+            VectorSnake trialFood = new VectorSnake(
+                    rand.nextInt(0,maxX),
+                    rand.nextInt(0,maxY));
+            boolean noImpact = true;
+            for (int i = 0; i < snake.size(); i++) {
+                if ((snake.get(i).x == trialFood.x) &&
+                        (snake.get(i).y == trialFood.y)) {
+                    noImpact = false;
+                }
+            }
+            if (noImpact) {
+                foodLocation = trialFood;
+                foodSpawned = true;
+                System.out.println("FOOD X: " + trialFood.x + " FOOD Y: " + trialFood.y);
+            }
+        }
+
+    }
+
+    private VectorSnake getNewSnakeHead() {
         int curHeadX = snake.getFirst().x;
         int curHeadY = snake.getFirst().y;
 
-        SnakeSegment newHead;
+        VectorSnake newHead;
         if (currentDirection == Direction.RIGHT) {
             if (lastMove != Direction.LEFT) {
-                newHead = new SnakeSegment(curHeadX + 1, curHeadY);
+                newHead = new VectorSnake(curHeadX + 1, curHeadY);
                 lastMove = Direction.RIGHT;
             } else {
-                newHead = new SnakeSegment(curHeadX - 1, curHeadY);
+                newHead = new VectorSnake(curHeadX - 1, curHeadY);
             }
         } else if (currentDirection == Direction.LEFT) {
             if (lastMove != Direction.RIGHT) {
-                newHead = new SnakeSegment(curHeadX - 1, curHeadY);
+                newHead = new VectorSnake(curHeadX - 1, curHeadY);
                 lastMove = Direction.LEFT;
             } else {
-                newHead = new SnakeSegment(curHeadX + 1, curHeadY);
+                newHead = new VectorSnake(curHeadX + 1, curHeadY);
             }
         } else if (currentDirection == Direction.UP) {
             if (lastMove != Direction.DOWN) {
-                newHead = new SnakeSegment(curHeadX, curHeadY - 1);
+                newHead = new VectorSnake(curHeadX, curHeadY - 1);
                 lastMove = Direction.UP;
             } else {
-                newHead = new SnakeSegment(curHeadX, curHeadY + 1);
+                newHead = new VectorSnake(curHeadX, curHeadY + 1);
             }
         } else { // it's going down.
             if (lastMove != Direction.UP) {
-                newHead = new SnakeSegment(curHeadX, curHeadY + 1);
+                newHead = new VectorSnake(curHeadX, curHeadY + 1);
                 lastMove = Direction.DOWN;
             } else {
-                newHead = new SnakeSegment(curHeadX, curHeadY - 1);
+                newHead = new VectorSnake(curHeadX, curHeadY - 1);
             }
         }
         collisionDetection(newHead);
         return newHead;
     }
 
-    private void collisionDetection(SnakeSegment movingSegment) {
-        // figuring out max "x" value to crash into right wall
-        int maxX = Options.SCREEN_WIDTH / snakeSize;
-        // figuring out max "y" value to crash into bottom wall
-        int maxY = Options.SCREEN_HEIGHT / snakeSize;
-        // test newest segment to see if it hit the walls:
+    private void collisionDetection(VectorSnake movingSegment) {
+        // compares max and min "x and y" values to crash into walls
         if ((movingSegment.x > maxX) || (movingSegment.x < 0) ||
             (movingSegment.y > maxY) || (movingSegment.y < 0)) {
             gameOver();
@@ -135,11 +169,21 @@ public class Game extends Displayable {
                 gameOver();
             }
         }
+
+        // test newest segment to see if it eats the food:
+
+        if (foodLocation != null) {
+            if ((foodLocation.x == movingSegment.x) &&
+                    (foodLocation.y == movingSegment.y)) {
+                foodSpawned = false;
+                foodLocation = null;
+                grow++;
+            }
+        }
     }
 
     private void gameOver() {
-        System.out.println("YOU LOSE, score: " + score);
-        Globals.ph.switchScene(Scene.MAIN);
+        goodGame = false;
     }
 
     @Override
@@ -148,6 +192,23 @@ public class Game extends Displayable {
         for (int i = 0; i < snake.size(); i++) {
             g.fillRect((snakeSize * snake.get(i).x) + 1, (snakeSize * snake.get(i).y) + 1,
                     snakeSize - 2, snakeSize - 2);
+        }
+        if (foodLocation != null) {
+            g.setColor(Color.ORANGE);
+            g.fillRect((snakeSize * foodLocation.x) + 1, (snakeSize * foodLocation.y) + 1,
+                    snakeSize - 2, snakeSize - 2);
+        }
+
+        if (!goodGame) {
+            FontMetrics fontMetrics = Toolkit.getDefaultToolkit().getFontMetrics(Globals.font);
+            int x = (Options.SCREEN_WIDTH - fontMetrics.stringWidth("YOU DIED")) / 2;
+            int y = (Options.SCREEN_HEIGHT + fontMetrics.getAscent()) / 2;
+
+            g.setColor(Color.RED);
+            g.setFont(Globals.font);
+            g.drawString("YOU DIED", x, y);
+            g.drawString("" + score , x, y + 40);
+
         }
     }
 }
--- src/game/VectorSnake.java
+++ src/game/VectorSnake.java
@@ -1,10 +1,10 @@
 package game;
 
-public class SnakeSegment {
+public class VectorSnake {
     public int x;
     public int y;
 
-    SnakeSegment(int x, int y) {
+    VectorSnake(int x, int y) {
         this.x = x;
         this.y = y;
     }
